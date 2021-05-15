97e66adb0c1bd36aa5a82c79c2d9b06d34df19c8
Andrew W. Burger <andrew.burger@awbtech.org>
2021-05-15 13:15:14 -0700

    Added Import Export to Admin

--- apps/main/admin.py
+++ apps/main/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Change
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
+# admin.site.register(Change)
 
-admin.site.register(Change)
+class ChangeResource(resources.ModelResource):
+    class Meta:
+        model = Change
 
+class ChangeAdmin(ImportExportModelAdmin):
+    resource_class = ChangeResource
+
+admin.site.register(Change, ChangeAdmin)
